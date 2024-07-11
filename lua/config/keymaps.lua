d7fff0b5290528d3f93102f7310363f64b96a1c8
antonio.costa <antonio.costa@nokia.com>
2024-07-11 15:41:08 +0100

    add mini-animate

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,7 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "oo", "o<Esc>", { noremap = true, silent = true })
-vim.keymap.set("n", "oi", "o", { noremap = true, silent = true })
-vim.keymap.set("n", "Oo", "O<Esc>", { noremap = true, silent = true })
-vim.keymap.set("n", "Oi", "O", { noremap = true, silent = true })
+local map = vim.keymap.set
+
+-- new lines
+map("n", "oo", "o<Esc>", { noremap = true, silent = true })
+map("n", "oi", "o", { noremap = true, silent = true })
+map("n", "Oo", "O<Esc>", { noremap = true, silent = true })
+map("n", "Oi", "O", { noremap = true, silent = true })
