64a501e05112e3cab2a398fb61c0344e2585e9db
Antonio <antonio.costa@nokia.com>
2025-03-25 17:17:12 +0000

    update norminette loadup to work with .h files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,18 +1,18 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "c", "make", "*.h" }, -- Add filetypes as needed
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.c", "*.h" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
+vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+  pattern = { "*.c", "*.h" },
   callback = function()
     vim.opt_local.expandtab = false
     vim.opt_local.shiftwidth = 4
     vim.opt_local.tabstop = 4
     vim.opt_local.softtabstop = 4
-  end,
-})
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = { "*.c", "*.h" },
-  callback = function()
-    vim.b.autoformat = false
   end,
 })
--- lua/plugins/norminette.lua
+++ lua/plugins/norminette.lua
@@ -1,8 +1,16 @@
 return {
   {
     dir = "~/.config/nvim/lua/norminette",
-    ft = { "c", "h" },
+    ft = { "c", "cpp" },
     config = function()
+      local filetype = vim.bo.filetype
+      local filename = vim.fn.expand("%:t") -- Get the filename
+
+      -- Only allow if it's a C file or a .h file
+      if filetype ~= "c" and not filename:match("%.h$") then
+        return
+      end
+
       package.loaded["norminette"] = nil
       require("norminette").setup()
     end,
